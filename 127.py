e728d026979418a9d5f7c2856227061f22593ee3
pyjuan001 <juancamilosernacortes12345@gmail.com>
2026-03-20 13:49:00 -0500

    Solution 127, 128

--- 127.py
+++ 127.py
@@ -1,8 +1,14 @@
 class Solution:
     def gcdOfStrings(self, str1: str, str2: str) -> str:
-        for i in str1:
-            
+        output = ""
+        if str1 + str2 == str2 + str1:
+            a = len(str1)
+            b = len(str2)
+            while b != 0:
+                a, b = b, a % b
 
+            return output+str1[:a]
+        return output
 
 s = Solution()
-print(s.gcdOfStrings("ABABAB", str2 = "ABAB"))
+print(s.gcdOfStrings("LEET", str2 = "CODE"))
