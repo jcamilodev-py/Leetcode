113ee57f61e2c6da9c86c2339a36fab53b31b58f
pyjuan001 <juancamilosernacortes12345@gmail.com>
2026-03-25 05:38:59 -0500

    Solution 136

---
+++ 136.py
@@ -0,0 +1,17 @@
+from typing import List
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        minimum = float('inf')
+        profit = 0
+        for i in prices:
+            if i < minimum:
+                minimum = i
+            if i - minimum > profit:
+
+                profit = i - minimum
+
+        return profit
+
+s = Solution()
+print(s.maxProfit([3,2]))
