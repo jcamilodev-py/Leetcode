e50b4e460365fbd8310c5d8aa3474231100706c4
pyjuan001 <juancamilosernacortes12345@gmail.com>
2026-04-01 17:00:43 -0500

    Solution 146

---
+++ 146.py
@@ -0,0 +1,11 @@
+class Solution:
+    def findPeaks(self, mountain: List[int]) -> List[int]:
+        ans = []
+        for i in range(1, len(mountain)-1):
+            if mountain[i] > mountain[i-1] and mountain[i] > mountain[i+1]:
+                ans.append(i)
+
+        return ans
+    
+s = Solution()
+print(s.findPeaks([2,4,4]))
