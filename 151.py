fe4225e7b1cf884149b4502471214ac39c352e31
pyjuan001 <juancamilosernacortes12345@gmail.com>
2026-04-03 18:57:05 -0500

    Solution 151

---
+++ 151.py
@@ -0,0 +1,21 @@
+from typing import List
+
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        jumps = 0
+        current_end = 0
+        farthest = 0
+
+        for i in range(len(nums) - 1):
+            farthest = max(farthest, i + nums[i])
+            print(farthest)
+            if i == current_end:
+                jumps += 1
+                current_end = farthest
+
+        return jumps
+
+
+
+s = Solution()
+print(s.jump([2,3,1,1,4]))
