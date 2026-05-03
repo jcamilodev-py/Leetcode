f5503eeeccaa26ab3f8407ba087b0b1600093e36
pyjuan001 <juancamilosernacortes12345@gmail.com>
2026-05-03 16:54:33 -0500

    Solution 006

--- 006.py
+++ 006.py
@@ -1,1 +1,15 @@
+class Solution:
+    def rotateString(self, s: str, goal: str) -> bool:
+        str = list(s)
+        for i in range(len(s)):
+            value = str[0]
+            del str[0]
+            str.append(value)
+            if "".join(str) == goal:
+                return True
+            
+        return False
 
+
+s = Solution()
+print(s.rotateString("aba", goal = "aab"))
