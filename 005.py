7a2d05867d9974c64ced99b825759e3de4f04a21
pyjuan001 <juancamilosernacortes12345@gmail.com>
2026-05-04 11:53:23 -0500

    solution improvement 005

--- 005.py
+++ 005.py
@@ -1,17 +1,15 @@
-class Solution(object):
-    def isPalindrome(self, x):
-        """
-        :type x: int
-        :rtype: bool
-        """
-        index = -1
-        x=str(x)
-        for i in range(0, len(x)):
-            if x[i] == x[index]:
-                index-=1
-            else:
-                return False
-        return True
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0 or (x % 10 == 0 and x != 0):
+            return False
+
+        reversed_half = 0
+        while x > reversed_half:
+            reversed_half = reversed_half * 10 + x % 10
+            x //= 10
+
+        return x == reversed_half or x == reversed_half // 10
+
 
 
 sol = Solution()
