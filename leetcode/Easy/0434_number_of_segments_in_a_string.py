443e4b7c0ced5763ab7dbc71425fc50ce3a8628a
pyjuan001 <juancamilosernacortes12345@gmail.com>
2026-06-30 16:37:24 -0500

    Remove old loose Easy/Medium/Hard folders

--- leetcode/Easy/0434_number_of_segments_in_a_string.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def countSegments(self, s: str) -> int:
-        s = s.split()
-        return len(s)
-
-
-s = Solution()
-print(s.countSegments("                "))
--- leetcode/Easy/2413_smallest_even_multiple.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def smallestEvenMultiple(self, n: int) -> int:
-        while n %2 != 0:
-            n*=2
-        return n
-
-
-s = Solution()
-print(s.smallestEvenMultiple(n = 6))
