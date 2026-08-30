71db22b08f2751aa1775bf65909bbf7ecb0b814e
pyjuan001 <juancamilosernacortes12345@gmail.com>
2026-08-30 16:08:08 -0500

    367. Valid Perfect Square

---
+++ leetcode/Python/Easy/367._Valid_Perfect_Square.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isPerfectSquare(self, num: int) -> bool:
+        l, r = 1, num
+
+        while l <= r:
+            m = (l + r) // 2
+
+            if m * m > num:
+                r = m - 1
+            elif m * m < num:
+                l = m + 1
+            else:
+                return True
+        return False
+            
+
+
+
+
+s = Solution()
+print(s.isPerfectSquare(2000105819))
