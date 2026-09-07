74d1ab73332debf6c02066baa1c3e8d21b5c7f21
pyjuan001 <juancamilosernacortes12345@gmail.com>
2026-09-07 14:33:27 -0500

    322. Coin Change

---
+++ leetcode/Python/Medium/322._Coin_Change.py
@@ -0,0 +1,22 @@
+from typing import List
+
+
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        dp = [float('inf')] * (amount+1)
+
+        dp[0] = 0
+
+        for i in range(1, amount+1):
+            for j in coins:
+                if i - j >=0:
+                    dp[i] = min(dp[i], dp[i-j]+1)
+
+ 
+        return dp[-1] if dp[-1] != float('inf') else -1
+
+
+
+
+s = Solution()
+print(s.coinChange([2], amount = 3))
