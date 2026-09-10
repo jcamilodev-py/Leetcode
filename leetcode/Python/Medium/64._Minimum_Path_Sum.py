c997021b2a2cbcad48f54305cd78325fd78a152d
jcamilo-py <juancamilosernacortes12345@gmail.com>
2026-09-10 18:54:00 -0500

    64. Minimum Path Sum

---
+++ leetcode/Python/Medium/64._Minimum_Path_Sum.py
@@ -0,0 +1,31 @@
+from typing import List
+
+class Solution:
+    def minPathSum(self, grid: List[List[int]]) -> int:
+        n = len(grid)
+        m = len(grid[0])
+
+        dp = [[0] * m for _ in range(n)]
+
+        dp[0][0] = grid[0][0]
+
+        for i in range(1, m):
+            dp[0][i] = dp[0][i-1] + grid[0][i]
+        
+        for i in range(1, n):
+            dp[i][0] = dp[i-1][0] + grid[i][0]
+
+        for i in range(1, n):
+            for j in range(1, m):
+                dp[i][j] = min(dp[i-1][j] + grid[i][j], dp[i][j-1] + grid[i][j])
+
+        return dp[-1][-1]
+
+
+
+
+
+
+
+s = Solution()
+print(s.minPathSum([[1,3,1],[1,5,1],[4,2,1]]))
